3de5a32c9baa0d979088ce4e20d727315776ea4a
John Heaven <john@johnheaven.eu>
2022-10-11 10:22:34 +0200

    added nearly final script

--- fraud-detection.py
+++ fraud-detection.py
@@ -6,79 +6,56 @@ from basic_functions import cust_dummies, custom_preprocess
 from basic_functions import get_data_from_csv, feature_engineering
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score, matthews_corrcoef
+import pandas as pd
+import pickle
 
 
 # #### load the model from disk
 # 
 # `loaded_model = joblib.load(filename)  
 # 
-# result = loaded_model.score(X_test, Y_test)  
+# result = loaded_model.score(X, Y_test)  
 
-df = get_data_from_csv() # input needs to be reworked, at the moment it takes 'data/xente/training.csv' as input
+df = get_data_from_csv(path='data/xente/full_test_export.csv') # input needs to be reworked, at the moment it takes 'data/xente/training.csv' as input
 df = feature_engineering(df)
 
-# --> pass in X_test as csv 
+X = df
+y = X.pop('FraudResult')
+
+# --> pass in X as csv 
 # --> pass in model as default, via model.sav and its location 
 # --> model.sav gives us y_pred = model.predict
 
-
 #BONUS: if function in case labels/target are present in test.csv --> print predictions and print score else: save predictions
 
-
-# sort test to match train
-X_test = X_test[["ProviderId", "ProductCategory", "ChannelId", "PricingStrategy", 
-        "weekday", "difference", "InOut", "Value", "time_of_day"]]
-
+# distinguish between numerical and categorical features
 cat_features = [
     "ProviderId", "ProductCategory", "ChannelId", "PricingStrategy", "InOut", "difference", "weekday"
     ]
 num_features = ["Value", "time_of_day"]
 
-_, X_test_sc = custom_preprocess(X_train_sm, X_test, nf=num_features)
+# import saved scaler so we can pass it into custom_preprocess
+our_scaler = pickle.load(open("pickles/scaler.p", "rb"))
+# for compatibility, we pass in X twice
+_, X_sc = custom_preprocess(X, X, nf=num_features, custom_scaler=our_scaler)
 
-X_test_sc = X_test_sc.astype({"difference": "object", "InOut": "object"})
-
-
-X_test_sc, cat_features = cust_dummies(X_test_sc, cat_features)
+# one hot encoding
+X_sc, cat_features = cust_dummies(X_sc, cat_features)
 
 ###########################
 #  Predict on test data   #
 ###########################
 
-# Calculating the accuracy for the RandomForest Classifier 
-print('Cross validation scores Random Forest:')
+# import a model from disk, as specified in command-line argument
+model_filename = 'pickles/rf_model.p'
+imported_model = pickle.load(open(model_filename, 'rb'))
+y_pred = imported_model.predict(X_sc)
+
+# Calculating the accuracy for the classifier
+print('Cross validation scores:')
 print('-------------------------')
-print("F1-score: {:.2f}".format(f1_score(y_test, y_test_rf)))
-print("MCC: {:.2f}".format(matthews_corrcoef(y_test, y_test_rf)))
+print("F1-score: {:.2f}".format(f1_score(y, y_pred)))
+print("MCC: {:.2f}".format(matthews_corrcoef(y, y_pred)))
 
 # Export results as CSV
-
-
-# some old code which I may want to reactivate again
-# 
-# `# Initiate OneHotEncoder()  
-# ohe = OneHotEncoder(handle_unknown='ignore')  
-# # run ohe  
-# X_train = ohe.fit_transform(X_train[cf])
-# X_test[cf] = ohe.transform(X_test[cf])
-#     
-# # transfrom sparse to dense
-# X_train = X_train.todense() 
-# X_test = X_test.todense()`
-# 
-
-# ### As learning a model takes a lot of time, I think it would be useful to save them to file so that they can be loaded later on for prediction
-# #### examplecode, save the model to disk
-# from [here](https://machinelearningmastery.com/save-load-machine-learning-models-python-scikit-learn/)  
-# 
-# `filename = 'finalized_model.sav'  
-# 
-# joblib.dump(model, filename)`  
-#  
-# #### some time later...
-#  
-# #### load the model from disk
-# 
-# `loaded_model = joblib.load(filename)  
-# 
-# result = loaded_model.score(X_test, Y_test)
+pd.DataFrame(y_pred).to_csv('output/predictions.csv')
